15463fc7af317f8a2c6bf489a2c688c23526020f
Duke Austin <dukeofaustintx@gmail.com>
2014-10-14 10:15:28 -0500

    Original commit
    test bed

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc4App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "\"One should not pursue goals that are easily achieved. One must develop an instinct for what one can just barely achieve through one's greatest efforts.\"  —Albert Einstein.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "What about what?  What's it to ya?";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
